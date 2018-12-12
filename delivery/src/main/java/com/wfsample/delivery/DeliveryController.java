6d97af6090799d7230d9331401234db39434fea7
Srujan Narkedamalli <snarkedamall@vmware.com>
2018-12-12 04:38:52 -0800

    Add more APIs, add latency and errors.

--- delivery/src/main/java/com/wfsample/delivery/DeliveryController.java
+++ delivery/src/main/java/com/wfsample/delivery/DeliveryController.java
@@ -5,6 +5,7 @@ import com.wfsample.common.dto.PackedShirtsDTO;
 import com.wfsample.service.DeliveryApi;
 
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.ws.rs.core.Response;
 
@@ -15,16 +16,49 @@ import javax.ws.rs.core.Response;
  * @author Hao Song (songhao@vmware.com).
  */
 public class DeliveryController implements DeliveryApi {
+  AtomicInteger tracking = new AtomicInteger(0);
+  AtomicInteger dispatch = new AtomicInteger(0);
+  AtomicInteger cancel = new AtomicInteger(0);
 
   @Override
   public Response dispatch(String orderNum, PackedShirtsDTO packedShirts) {
-    if (packedShirts == null || packedShirts.getShirts() == null ||
-        packedShirts.getShirts().size() == 0) {
+    if (dispatch.incrementAndGet() % 20 == 0) {
       return Response.status(Response.Status.BAD_REQUEST).entity(
           new DeliveryStatusDTO(null, "no shirts to deliver")).build();
+    }
+    try {
+      Thread.sleep(90);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
     String trackingNum = UUID.randomUUID().toString();
     System.out.println("Tracking number of Order:" + orderNum + " is " + trackingNum);
     return Response.ok(new DeliveryStatusDTO(trackingNum, "shirts delivery dispatched")).build();
+  }
+
+  @Override
+  public Response trackOrder(String orderNum) {
+    if (tracking.incrementAndGet() % 8 == 0) {
+      return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+    try {
+      Thread.sleep(30);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    return Response.ok().build();
+  }
+
+  @Override
+  public Response cancelOrder(String orderNum) {
+    try {
+      Thread.sleep(45);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    if (cancel.incrementAndGet() % 7 == 0) {
+      return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+    return Response.ok().build();
   }
 }
--- packaging/src/main/java/com/wfsample/packaging/PackagingService.java
+++ packaging/src/main/java/com/wfsample/packaging/PackagingService.java
@@ -1,10 +1,19 @@
 package com.wfsample.packaging;
 
+import com.google.protobuf.ByteString;
+
+import com.wfsample.beachshirts.GiftPack;
 import com.wfsample.beachshirts.PackagingGrpc;
 import com.wfsample.beachshirts.PackedShirts;
+import com.wfsample.beachshirts.Void;
 import com.wfsample.beachshirts.WrapRequest;
+import com.wfsample.beachshirts.WrappingType;
+import com.wfsample.beachshirts.WrappingTypes;
 import com.wfsample.common.BeachShirtsUtils;
 import com.wfsample.common.GrpcServiceConfig;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
@@ -34,10 +43,20 @@ public class PackagingService {
   }
 
   static class PackagingImpl extends PackagingGrpc.PackagingImplBase {
+    private final AtomicInteger getTypes = new AtomicInteger(0);
+    private final AtomicInteger restock = new AtomicInteger(0);
+    private final AtomicInteger wrap = new AtomicInteger(0);
+    private final AtomicInteger giftWrap = new AtomicInteger(0);
+    private final Random random = new Random();
 
     @Override
     public void wrapShirts(WrapRequest request, StreamObserver<PackedShirts> responseObserver) {
-      if (request.getShirtsCount() > 10) {
+      try {
+        Thread.sleep(55);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      if (wrap.incrementAndGet() % 40 == 0) {
         // can't pack more than 10 shirts at once.
         responseObserver.onError(Status.INTERNAL.asRuntimeException());
       }
@@ -45,6 +64,68 @@ public class PackagingService {
           addAllShirts(request.getShirtsList()).
           build());
       responseObserver.onCompleted();
+    }
+
+    @Override
+    public void giftWrap(WrapRequest request, StreamObserver<GiftPack> responseObserver) {
+      if (giftWrap.incrementAndGet() % 30 == 0) {
+        responseObserver.onError(Status.INTERNAL.asRuntimeException());
+      }
+      try {
+        Thread.sleep(70);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      if (request.getShirtsCount() > 40) {
+        int resp = (int) Math.round(random.nextDouble() *
+            100.0) + 10000;
+        try {
+          Thread.sleep(1400);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        ByteString byteString = ByteString.copyFrom(new byte[resp]);
+        responseObserver.onNext(GiftPack.newBuilder().setGiftMaterial(byteString).build());
+        responseObserver.onCompleted();
+      } else {
+        int resp = (int) Math.round(random.nextDouble() * 100.0);
+        ByteString byteString = ByteString.copyFrom(new byte[resp]);
+        responseObserver.onNext(GiftPack.newBuilder().setGiftMaterial(byteString).build());
+        responseObserver.onCompleted();
+      }
+    }
+
+    @Override
+    public void restockMaterial(WrappingType request,
+                                StreamObserver<com.wfsample.beachshirts.Status> responseObserver) {
+      try {
+        Thread.sleep(40);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      if (restock.incrementAndGet() % 20 == 0) {
+        responseObserver.onError(Status.INTERNAL.asRuntimeException());
+      } else {
+        responseObserver.onNext(com.wfsample.beachshirts.Status.newBuilder().
+            setStatus(true).build());
+        responseObserver.onCompleted();
+      }
+    }
+
+    @Override
+    public void getPackingTypes(Void request, StreamObserver<WrappingTypes> responseObserver) {
+      try {
+        Thread.sleep(60);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      if (getTypes.incrementAndGet() % 30 == 0) {
+        responseObserver.onError(Status.INTERNAL.asRuntimeException());
+      } else {
+        responseObserver.onNext(WrappingTypes.newBuilder().addWrappingType(WrappingType.
+            newBuilder().setWrappingType("wrap wrap wrap wrap").build()).build());
+        responseObserver.onCompleted();
+      }
     }
   }
 }
